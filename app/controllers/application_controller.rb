29a5711cac669f5704f99c6fc2722a4db466b11e
kasiaya <ayano.kashimoto@gmail.com>
2024-09-08 21:57:25 +0900

    スタッフ認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_staff! #ログインしていないユーザーをログインページの画面に促す
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_id, :department, :staff_name, :staff_name_kana])
+  end
+
 end
