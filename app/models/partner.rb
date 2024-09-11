f806f905e647867fda11c1ff93abcde595a862d1
kasiaya <ayano.kashimoto@gmail.com>
2024-09-11 11:55:42 +0900

    バリデーション設定

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,11 @@
 class Partner < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :institution_category
+
+  #医療機関名を空で保存できないようにする
+  validates :institution_name, :string, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :institution_category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
