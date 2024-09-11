b5df882858ee4055cd78667454a42c4641f1ee47
kasiaya <ayano.kashimoto@gmail.com>
2024-09-11 14:00:42 +0900

    エラーメッセージ

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -3,7 +3,7 @@ class Partner < ApplicationRecord
   belongs_to :institution_category
 
   #医療機関名を空で保存できないようにする
-  validates :institution_name, :string, presence: true
+  validates :institution_name, presence: true
 
   #ジャンルの選択が「---」の時は保存できないようにする
   validates :institution_category_id, numericality: { other_than: 1 , message: "can't be blank"} 
