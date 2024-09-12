5597a839d3793d96065fd10730de9eecc482b60f
kasiaya <ayano.kashimoto@gmail.com>
2024-09-12 15:10:09 +0900

    Doctorsモデル単体テスト

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @doctor = FactoryBot.build(:doctor)
+  end
+
+  describe '担当医新規登録' do
+    context '新規登録できるとき' do
+      it '担当医名、診療科が存在し登録可能' do
+        expect(@doctor).to be_valid
+      end
+    end
+    context '新規登録できないとき' do
+      it '医療機関名が空では登録できない' do
+        @doctor.doctor_name = ""
+        @doctor.valid?
+        expect(@doctor.errors.full_messages).to include("Doctor name can't be blank")
+      end
+      it '医療機関分類が空では登録できない' do
+        @doctor.specialty_id = ""
+        @doctor.valid?
+        expect(@doctor.errors.full_messages).to include("Specialty can't be blank")
+      end
+    end
+  end
 end
