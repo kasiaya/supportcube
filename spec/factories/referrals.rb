b96bba25e09ab1e4b562e234ccd2336502869cd5
kasiaya <ayano.kashimoto@gmail.com>
2024-09-17 14:03:16 +0900

    Referralsモデル単体テスト

--- spec/factories/referrals.rb
+++ spec/factories/referrals.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :referral do
-    
+    patient_name  {'鈴木一朗'}
+    age  {1}
+    gender  {1}
+    adl  {"全介助"}
+    history  {1}
+    purpose  {1}
+    insurance  {1}
+    prognosis  {"自宅"}
+    status  {0}
+    determination  {"preparing"}
+    memo  {"最短希望"}
+
+    association :staff
+    association :doctor
+    association :partner
   end
 end
