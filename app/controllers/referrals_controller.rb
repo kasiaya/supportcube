ec5657b973f129db07785dd46ab392ba759d8aca
kasiaya <ayano.kashimoto@gmail.com>
2024-09-19 17:29:32 +0900

    編集画面への変遷

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -23,6 +23,12 @@ class ReferralsController < ApplicationController
     @referral = Referral.includes(:doctor, :partner).find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def set_staff
